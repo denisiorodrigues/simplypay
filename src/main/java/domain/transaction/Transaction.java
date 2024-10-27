26878c05d5ced85c029b2546b378fddff106ebe1
Denisio Rordrigues <denisio@ymail.com>
2024-10-27 17:50:47 -0300

    Add transaction

---
+++ src/main/java/domain/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package domain.transaction;
+
+import domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "reciver_id")
+    private User recivre;
+    private LocalDateTime createAt;
+
+}
