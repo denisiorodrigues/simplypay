96b723b8967b07579f43d806636034e0c31d1dce
Denisio Rordrigues <denisio@ymail.com>
2024-11-12 20:10:16 -0300

    feat(test): throw exception when transaction is not allowed

--- src/test/java/com/dentech/simplypay/services/TransactionServiceTest.java
+++ src/test/java/com/dentech/simplypay/services/TransactionServiceTest.java
@@ -4,6 +4,7 @@ import com.dentech.simplypay.domain.user.User;
 import com.dentech.simplypay.domain.user.UserType;
 import com.dentech.simplypay.dtos.TransactionDto;
 import com.dentech.simplypay.repositories.TransactionRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,7 +38,7 @@ class TransactionServiceTest {
 
     @BeforeEach
     void beforeEach(){
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -67,7 +68,19 @@ class TransactionServiceTest {
 
     @Test
     @DisplayName("Should throw exception when transaction is not allowed")
-    void throwsAnExceptionWhenNotAllowed() {
+    void throwsAnExceptionWhenNotAllowed() throws Exception {
+        User sender = new User(1L,"Maria", "Sousa", "00000000099", "maria@test.com", "123456", new BigDecimal(1000), UserType.COMMON);
+        User receiver = new User(2L, "Jose", "Sousa", "00000000099", "jose@test.com", "123456", new BigDecimal(1000), UserType.COMMON);
 
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+        when(authorizationService.authorizeTransaction(sender, new BigDecimal(1000))).thenReturn(false);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            TransactionDto request = new TransactionDto(new BigDecimal(1000), 1L, 2L);
+            this.transactionService.createTransaction(request);
+        });
+
+        Assertions.assertEquals("Transação não autorizada", thrown.getMessage());
     }
 }
